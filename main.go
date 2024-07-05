799fd7fb2ce2428945659027cf1a1a3c7c6bc1dc
askrishna98 <99672787+askrishna98@users.noreply.github.com>
2024-07-05 18:14:35 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/askrishna98/library_/handlers"
-	"github.com/askrishna98/library_/loaddata"
-	"github.com/askrishna98/library_/models"
-	service "github.com/askrishna98/library_/services"
 )
 
 func main() {
-	// should initiate DB first
-	DB := models.GetMockDBInstance()
-	Id := service.InitalizeIDGenerator()
-
-	loaddata.LoadData(DB, Id)
-
-	for _, val := range DB.Books {
-		fmt.Println(*val)
-	}
-	for _, val := range DB.Members {
-		fmt.Println(*val)
-	}
+	// start App
 	handlers.StartApp()
 
 }
